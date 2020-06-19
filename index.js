682b68293b8ac384ee8584803d084e2c6c96e4c8
Nhien Dang <danghaonhien@gmail.com>
2020-06-18 18:37:10 -0700

    heroku version

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
